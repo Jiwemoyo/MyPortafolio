85a6feb9feb5587151127bee0ae26dd4079f940f
Aless4ndr0 <jiwemoyo@gmail.com>
2024-11-12 12:25:29 -0500

    navbar propia

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,79 +7,56 @@ import {
 } from "react-icons/fa";
 
 export const SideBar = () => {
-    const [activeItem, setActiveItem] = useState(1);  // Estado para el item clickeado
-    const [hoveredItem, setHoveredItem] = useState(1);  // Estado para el item en hover
-  
-    const handleClick = (index) => {
-      // Si se hace clic en el mismo item, lo desactiva, si no, activa el nuevo item
-      setActiveItem(activeItem === index ? null : index);
-    };
+  const [textIcon, setTextIcon] = useState(null);
+  const [cliked, setCliked] = useState('home');
+
+  const mauseEntered = (item) => {
+    setTextIcon(item);
+  };
+  const mauseExit = () => setTextIcon(null);
+
+  const clikeado = (item) => {
+    setCliked(item);
+  };
 
   return (
-    <nav className="w-24 h-full bg-slate-400 fixed top-0 left-0 flex items-center justify-center">
-      <ul className="flex flex-col items-center space-y-8 py-8">
-        <li 
-          className="text-white flex flex-col items-center cursor-pointer" 
-          onClick={() => handleClick(1)}
-          onMouseEnter={() => setHoveredItem(1)}  // Mostrar texto en hover
-          onMouseLeave={() => setHoveredItem(null)}  // Ocultar texto cuando se sale del hover
+    <nav className=" bg-slate-400 py-4 absolute bottom-0 w-full sm:w-32 sm:h-full sm:fixed sm:top-0 sm:left-0 sm:flex sm:items-center sm:justify-center">
+      <ul className=" flex justify-evenly gap-8 sm:flex-col sm:gap-16">
+        <li
+          className="text-white cursor-pointer hover:scale-125 transition duration-200 flex flex-col justify-center items-center font-bold"
+          onMouseEnter={() => mauseEntered("home")}
+          onMouseLeave={mauseExit}
+          onClick={()=>clikeado("home")}
         >
-          <FaHome size={30} 
-            className={`transition-transform duration-300 transform ${activeItem === 1 || hoveredItem === 1 ? 'scale-100' : 'scale-125'} hover:scale-100`} 
-          />
-          <span 
-            className={`mt-2 transition-opacity duration-300 ${activeItem === 1 || hoveredItem === 1 ? 'opacity-100' : 'opacity-0'} hover:opacity-100`}
-          >
-            Home
-          </span>
+          <FaHome size={30} />
+          {(textIcon === 'home' || cliked === 'home') && <span className=" text-xs">Home</span>}
         </li>
-
-        <li 
-          className="text-white flex flex-col items-center cursor-pointer" 
-          onClick={() => handleClick(2)}
-          onMouseEnter={() => setHoveredItem(2)}  // Mostrar texto en hover
-          onMouseLeave={() => setHoveredItem(null)}  // Ocultar texto cuando se sale del hover
+        <li
+          className="text-white cursor-pointer hover:scale-125 transition duration-200 flex flex-col justify-center items-center font-bold"
+          onMouseEnter={() => mauseEntered("about")}
+          onMouseLeave={mauseExit}
+          onClick={()=>clikeado("about")}
         >
-          <FaInfoCircle size={30} 
-            className={`transition-transform duration-300 transform ${activeItem === 2 || hoveredItem === 2 ? 'scale-100' : 'scale-125'} hover:scale-100`} 
-          />
-          <span 
-            className={`mt-2 transition-opacity duration-300 ${activeItem === 2 || hoveredItem === 2 ? 'opacity-100' : 'opacity-0'} hover:opacity-100`}
-          >
-            About
-          </span>
+          <FaInfoCircle size={30} />
+          {(textIcon === 'about' || cliked === 'about') && <span className=" text-xs">About</span>}
         </li>
-
-        <li 
-          className="text-white flex flex-col items-center cursor-pointer" 
-          onClick={() => handleClick(3)}
-          onMouseEnter={() => setHoveredItem(3)}  // Mostrar texto en hover
-          onMouseLeave={() => setHoveredItem(null)}  // Ocultar texto cuando se sale del hover
+        <li
+          className="text-white cursor-pointer hover:scale-125 transition duration-200 flex flex-col justify-center items-center font-bold"
+          onMouseEnter={() => mauseEntered("project")}
+          onMouseLeave={mauseExit}
+          onClick={()=>clikeado("project")}
         >
-          <FaProjectDiagram size={30} 
-            className={`transition-transform duration-300 transform ${activeItem === 3 || hoveredItem === 3 ? 'scale-100' : 'scale-125'} hover:scale-100`} 
-          />
-          <span 
-            className={`mt-2 transition-opacity duration-300 ${activeItem === 3 || hoveredItem === 3 ? 'opacity-100' : 'opacity-0'} hover:opacity-100`}
-          >
-            Projects
-          </span>
+          <FaProjectDiagram size={30} />
+          {(textIcon === 'project' || cliked === 'project') && <span className=" text-xs">Projects</span>}
         </li>
-
-        <li 
-          className="text-white flex flex-col items-center cursor-pointer" 
-          onClick={() => handleClick(4)}
-          onMouseEnter={() => setHoveredItem(4)}  // Mostrar texto en hover
-          onMouseLeave={() => setHoveredItem(null)}  // Ocultar texto cuando se sale del hover
+        <li
+          className="text-white cursor-pointer hover:scale-125 transition duration-200 flex flex-col justify-center items-center font-bold"
+          onMouseEnter={() => mauseEntered("contact")}
+          onMouseLeave={mauseExit}
+          onClick={()=>clikeado("contact")}
         >
-          <FaPhoneAlt size={30} 
-            className={`transition-transform duration-300 transform ${activeItem === 4 || hoveredItem === 4 ? 'scale-100' : 'scale-125'} hover:scale-100`} 
-          />
-          <span 
-            className={`mt-2 transition-opacity duration-300 ${activeItem === 4 || hoveredItem === 4 ? 'opacity-100' : 'opacity-0'} hover:opacity-100`}
-          >
-            Contact
-          </span>
+          <FaPhoneAlt size={30} />
+          {(textIcon === 'contact' || cliked === 'contact') && <span className=" text-xs">Contact</span>}
         </li>
       </ul>
     </nav>
