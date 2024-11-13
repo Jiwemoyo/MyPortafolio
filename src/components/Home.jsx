b3b28b6b07c932159e959c35cf344bd691c0f01a
Aless4ndr0 <jiwemoyo@gmail.com>
2024-11-13 12:07:16 -0500

    home pantallas pequenas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,61 @@
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className=" flex flex-col items-center justify-center h-screen text-center  bg-custom-blue gap-5 px-8">
+      <h1 className=" text-custom-celeste font-bold text-3xl">
+        Hi, I'm Alessandro
+      </h1>
+      <h2 className=" text-custom-red font-bold text-2xl">
+        Web Developer & Designer
+      </h2>
+      <p className=" font-bold text-custom-white">
+        Welcome to my digital universe, where creativity and technology merge to
+        create unique experiences.
+      </p>
+      <li className="grid grid-cols-3 gap-8 text-custom-white mt-8 font-semibold">
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          Html
+        </ul>
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          CSS
+        </ul>
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          JS
+        </ul>
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          React
+        </ul>
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          Tailwind
+        </ul>
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          npm
+        </ul>
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          Python
+        </ul>
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          Flask
+        </ul>
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          SQL
+        </ul>
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          MongoDB
+        </ul>
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          FireBase
+        </ul>
+        <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
+          Figma
+        </ul>
+        <ul className=" col-start-2 animate-bg-fade bg-blue-500 p-2 rounded-lg">
+          Blender
+        </ul>
+      </li>
+      <div className=" w-full flex justify-between gap-5 mt-5 ">
+        <button className=" font-bold text-custom-white bg-custom-red px-8 py-4 rounded-lg">Projects</button>
+        <button className=" font-bold text-custom-white bg-custom-red px-8 py-4 rounded-lg">Contact Me</button>
+      </div>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ export default {
     extend: {
       colors:{
         'custom-blue':'#2B3467',
-        'custom-white':'#FCFFE7'
+        'custom-white':'#FCFFE7',
+        'custom-celeste':'#BAD7E9FF',
+        'custom-red':'#EB455F'
+      },
+      animation:{
+        'bg-fade': 'bgFade 2s ease-in-out infinite',
+      },
+      keyframes:{
+        bgFade: {
+          '0%, 100%': { backgroundColor: 'rgba(186, 215, 233,0.2)' }, // transparente
+          '50%': { backgroundColor: 'rgb(186, 215, 233,0.6)' }, // color con opacidad (ajústalo según lo que necesites)
+        },
       }
     },
   },
