46055b1095109b9e24861cea1f951a5767c299df
Aless4ndr0 <jiwemoyo@gmail.com>
2024-11-15 12:21:24 -0500

    home corregido y abour comenzado

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,63 +1,38 @@
 import { useTranslation } from "react-i18next";
 
 export const Home = () => {
-
-  const {t}=useTranslation()
+  const { t } = useTranslation();
 
   return (
     <div className="bg-custom-blue flex justify-center items-center h-screen">
       <div className="flex flex-col items-center justify-center h-screen gap-5 px-8 text-center sm:items-start sm:gap-11">
         <h1 className=" text-custom-celeste font-bold text-3xl sm:text-9xl">
-          {t('greetings')}
+          {t("greetings")}
         </h1>
         <h2 className=" text-custom-red font-bold text-2xl sm:text-5xl">
-          {t('profetion')}
+          {t("profetion")}
         </h2>
         <p className=" font-bold text-custom-white sm:text-2xl">
           {t("presentation")}
         </p>
-        <li className="grid grid-cols-3 gap-8 text-custom-white mt-5 font-semibold sm:flex">
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">Html</ul>
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">CSS</ul>
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">JS</ul>
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
-            React
-          </ul>
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
-            Tailwind
-          </ul>
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">npm</ul>
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
-            Python
-          </ul>
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
-            Flask
-          </ul>
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">SQL</ul>
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
-            MongoDB
-          </ul>
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
-            FireBase
-          </ul>
-          <ul className=" animate-bg-fade bg-blue-500 p-2 rounded-lg ">
-            Figma
-          </ul>
-          <ul className=" col-start-2 animate-bg-fade bg-blue-500 p-2 rounded-lg">
+        <ul className="grid grid-cols-3 gap-8 text-custom-white mt-5 font-semibold sm:flex">
+          <li className="animate-bg-fade bg-blue-500 p-2 rounded-lg">{t('skills-home.DS')}</li>
+          <li className="animate-bg-fade bg-blue-500 p-2 rounded-lg">{t('skills-home.wD')}</li>
+          <li className="animate-bg-fade bg-blue-500 p-2 rounded-lg">{t('skills-home.3D')}</li>    
+          {/* <li className="col-start-2 animate-bg-fade bg-blue-500 p-2 rounded-lg">
             Blender
-          </ul>
-        </li>
+          </li> */}
+        </ul>
+
         <div className=" w-full flex justify-between gap-5 mt-5 sm:w-auto sm:gap-11">
-        <button className=" font-bold text-custom-white bg-custom-red px-8 py-4 rounded-lg">
-          {t('button-Contact')}
-        </button>
-        <button className=" font-bold text-custom-white bg-custom-red px-8 py-4 rounded-lg">
-          {t('button-Projects')}
-        </button>
+          <button className=" font-bold text-custom-white bg-custom-red px-8 py-4 rounded-lg">
+            {t("button-Contact")}
+          </button>
+          <button className=" font-bold text-custom-white bg-custom-red px-8 py-4 rounded-lg">
+            {t("button-Projects")}
+          </button>
+        </div>
       </div>
-      </div>
-
-
     </div>
   );
 };
