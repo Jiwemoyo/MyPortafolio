16abcb6c36919bf113f0089961cb818a86b7015c
Aless4ndr0 <jiwemoyo@gmail.com>
2024-11-26 13:11:36 -0500

    configuracion inicial

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export const About = () => {
   return (
     <div className="bg-custom-blue flex flex-col gap-6 justify-center items-center h-screen px-8 text-center text-custom-celeste text-xl font-semibold sm:text-2xl sm:w-full sm:px-96">
       <p>{t("about-presentation")}</p>
-      <h3 className="text-custom-red text-2xl sm:text-3xl mt-5">{t("carer-path")}</h3>
+      <h3 className="text-custom-red text-2xl font-bold sm:text-5xl mt-5">{t("carer-path")}</h3>
       <ul className="list-disc text-xl px-6 text-left text-custom-white space-y-4 sm:text-2xl">
         <li>
           <span className="text-custom-celeste mr-8">2019 - 2020</span>
@@ -21,7 +21,7 @@ export const About = () => {
           {t("dates-about.date3")}
         </li>
       </ul>
-      <h3 className="text-custom-red text-2xl mt-5 sm:text-3xl">{t("skills")}</h3>
+      <h3 className="text-custom-red text-2xl font-bold mt-5 sm:text-5xl">{t("skills")}</h3>
       <ul className="grid grid-cols-3 gap-8 text-custom-white mt-5 font-semibold sm:flex sm:flex-wrap">
         {[
           "Html", "CSS", "JS", "React", "Tailwind", "npm", 
@@ -32,7 +32,7 @@ export const About = () => {
           </li>
         ))}
       </ul>
-      <h3 className="text-custom-red text-2xl mt-5 sm:text-3xl">{t("Philosophy")}</h3>
+      <h3 className="text-custom-red text-2xl font-bold mt-5 sm:text-5xl">{t("Philosophy")}</h3>
       <p className="bg-custom-red py-8 px-8 bg-opacity-25 rounded-lg">{t("Philosophy-context")}</p>
     </div>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,7 +7,7 @@ export const Projects = () => {
   const { t } = useTranslation();
   return (
 <div className="bg-custom-blue flex flex-col gap-8 justify-center items-center h-100 px-8 text-center text-custom-celeste text-xl font-semibold sm:text-2xl sm:w-screen sm:py-28">
-      <h2 className="text-custom-red text-3xl lg:text-4xl">{t('Proyects-title')}</h2>
+      <h2 className="text-custom-red text-3xl font-bold sm:text-5xl">{t('Proyects-title')}</h2>
       <p className="max-w-2xl">
         {t('Proyects-presentation')}
       </p>
