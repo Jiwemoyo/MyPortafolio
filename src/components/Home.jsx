ac7ffb92626d17c897d77825281cdd61db7ebd01
Aless4ndr0 <jiwemoyo@gmail.com>
2024-12-01 11:22:42 -0500

    linkeando cosas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   const { t } = useTranslation();
@@ -16,21 +17,40 @@ export const Home = () => {
           {t("presentation")}
         </p>
         <ul className="grid grid-cols-3 gap-8 text-custom-white mt-5 font-semibold sm:flex">
-          <li className="animate-bg-fade bg-blue-500 p-2 rounded-lg">{t('skills-home.DS')}</li>
-          <li className="animate-bg-fade bg-blue-500 p-2 rounded-lg">{t('skills-home.wD')}</li>
-          <li className="animate-bg-fade bg-blue-500 p-2 rounded-lg">{t('skills-home.3D')}</li>    
+          <li className="animate-bg-fade bg-blue-500 p-2 rounded-lg">
+            {t("skills-home.DS")}
+          </li>
+          <li className="animate-bg-fade bg-blue-500 p-2 rounded-lg">
+            {t("skills-home.wD")}
+          </li>
+          <li className="animate-bg-fade bg-blue-500 p-2 rounded-lg">
+            {t("skills-home.3D")}
+          </li>
           {/* <li className="col-start-2 animate-bg-fade bg-blue-500 p-2 rounded-lg">
             Blender
           </li> */}
         </ul>
 
         <div className=" w-full flex justify-between gap-5 mt-5 sm:w-auto sm:gap-11">
-          <button className=" font-bold text-custom-white bg-custom-red px-8 py-4 rounded-lg">
-            {t("button-Contact")}
-          </button>
-          <button className=" font-bold text-custom-white bg-custom-red px-8 py-4 rounded-lg">
-            {t("button-Projects")}
-          </button>
+          <Link to="/contact">
+            <button
+              className="font-bold text-custom-white bg-custom-red px-8 py-4 rounded-lg
+                    transform transition-all duration-300 hover:-translate-y-1 hover:shadow-lg
+                    hover:shadow-custom-celeste/20 hover:bg-custom-celeste hover:text-custom-blue"
+            >
+              {t("button-Contact")}
+            </button>
+          </Link>
+
+          <Link to="/projects">
+            <button
+              className="font-bold text-custom-white bg-custom-red px-8 py-4 rounded-lg
+                    transform transition-all duration-300 hover:-translate-y-1 hover:shadow-lg 
+                    hover:shadow-custom-celeste/20 hover:bg-custom-celeste hover:text-custom-blue"
+            >
+              {t("button-Projects")}
+            </button>
+          </Link>
         </div>
       </div>
     </div>
